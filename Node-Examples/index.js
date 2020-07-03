ae8f407a3f3a939f28070b03a76c8a660cca14f0
Himanshu Rathi <himanshu0998@gmail.com>
2020-07-03 11:55:11 +0530

    Node Callbacks and Error Handling and Asynchronous Computation Example

--- Node-Examples/index.js
+++ Node-Examples/index.js
@@ -3,14 +3,20 @@ var rect = require('./rectangle')  //rectangle.js is thus a node module
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = "
-               + l + ",  and b = " + b);
-    }
-    else {
-	    console.log("The area of the rectangle is " + rect.area(l,b));
-	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-    }
+    rect(l,b, (err, rectangle) => {
+        if(err)
+        {
+            console.log("ERROR: ", err.message);
+        }
+        else
+        {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()"); // This will execute within 2 sec delay.. this shows asynchronous computation
 }
 
 solveRect(2,4);
