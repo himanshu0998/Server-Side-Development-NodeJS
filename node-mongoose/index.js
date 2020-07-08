dd6aab120d2dbcd6144d937088fc57420369991a
Himanshu Rathi <himanshu0998@gmail.com>
2020-07-08 15:17:57 +0530

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -15,11 +15,23 @@ connect.then((db)=>{
     .then((dish) => {
         console.log(dish);
         
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set: { description: 'Updated Test'}
+        },{
+            new : true  //this 'new' flag updates and then returns the same dish again when set
+        }).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
-
+    .then((dish) => {
+        console.log(dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
         return Dishes.remove({});
     })
     .then(() => {
