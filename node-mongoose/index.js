fa1483656025d178e8cbf5873a5cd9977632f451
Himanshu Rathi <himanshu0998@gmail.com>
2020-07-08 15:00:50 +0530

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -6,24 +6,48 @@ const connect = mongoose.connect(url);
 
 connect.then((db)=>{
     console.log('Connected Correctly to server');
-    var newDish = new Dishes({
-        name: 'Uthappizza',
-        description: 'test'
-    });
+    
+// create method can directly be used to create and save in the db as shown
+    Dishes.create({
+        name: 'Uthapizza',
+        description: 'Test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
 
-    newDish.save()   //saves the newDish and returns a Promise 
-        .then((dish)=>{
-            console.log(dish);
-            return Dishes.find({}).exec() //executes find query on Dishes collecion created by mongoose and returns a Promise..hence nested then
-        })
-        .then((dishes)=>{
-            console.log(dishes);
-            return Dishes.remove({});
-        })
-        .then(()=>{
-            return mongoose.connection.close();
-        })
-        .catch((err)=>{
-            console.log(err);
-        });
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+    
+// A way to create a new Dish and save in the db
+    // var newDish = new Dishes({
+    //     name: 'Uthappizza',
+    //     description: 'test'
+    // });
+
+    // newDish.save()   //saves the newDish and returns a Promise 
+    //     .then((dish)=>{
+    //         console.log(dish);
+    //         return Dishes.find({}).exec() //executes find query on Dishes collecion created by mongoose and returns a Promise..hence nested then
+    //     })
+    //     .then((dishes)=>{
+    //         console.log(dishes);
+    //         return Dishes.remove({});
+    //     })
+    //     .then(()=>{
+    //         return mongoose.connection.close();
+    //     })
+    //     .catch((err)=>{
+    //         console.log(err);
+    //     });
 });
