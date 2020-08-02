71cfe7c8774f54af8627445e68d798884e0503fd
Himanshu Rathi <himanshu0998@gmail.com>
2020-08-02 13:00:24 +0530

    Passport-Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey':'12345-67890-09876-54321',
-    'mongoURL':'mongodb://localhost:27017/conFusion'
+    'mongoURL':'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId:'325462238828777',
+        clientSecret:'461115f811466bfa9c924ecc47e1df55'
+    }
 }
